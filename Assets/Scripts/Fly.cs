5b2aa5792962e51e2b93f1e24baa38d27fd0264f
Pedro Braga <phbm.braga@gmail.com>
2021-04-07 17:37:09 -0300

    Moving from 2D to 3D

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public float speed = 100.0f;
+    public float rotationSpeed = 100.0f;
+
+    Vector3 up;
+    Vector3 forward;
+    Vector3 side;
+
+    void Update()
+    {
+        side = transform.right;
+        up = transform.up;
+        forward = transform.forward;
+
+        float translateX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float translateY = Input.GetAxis("VerticalY") * speed * Time.deltaTime;
+        float translateZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        // This makes the airplane goes up and down on its own axis, because I multiply the translate by each axis normalized
+        // before adding to transform.position (inside HolisticMath.Translate()):
+
+        //transform.position = HolisticMath.Translate(transform.position, side, translateX);
+        //transform.position = HolisticMath.Translate(transform.position, up, translateY);
+        //transform.position = HolisticMath.Translate(transform.position, forward, translateZ);
+
+        // Unity's built in translate also do the same :
+
+        transform.Translate(translateX, 0, 0);
+        transform.Translate(0, translateY, 0);
+        transform.Translate(0, 0, translateZ);
+
+        // This makes the airplane goes up and down independent of its own rotations, because here I only add a value on each
+        // componente of transform.position (independent of changes in transform.right, transform.up and transform.forward):
+
+        //transform.position = new Vector3(transform.position.x + translateX,
+        //                                    transform.position.y + translateY,
+        //                                    transform.position.z + translateZ);
+    }
+}
